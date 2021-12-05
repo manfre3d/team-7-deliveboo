ffce899d1e115b59e154482040ec11e4190ef006
eliaraguso <elia.raguso@gmail.com>
2021-12-05 11:40:53 +0100

    Inserimento Seeder per Restaurants_types e Orders

---
+++ database/seeds/OrdersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Order;
+use Faker\Generator as Faker;
+
+
+class OrdersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ( $i = 0; $i < 50; $i++) {
+            $newOrder = new Order();
+            $newOrder->user_id = $faker->numberBetween(1, 15) ;
+            $newOrder->total_price = $faker->randomFloat(2, 1, 999);
+            $newOrder->customer_name = $faker->firstName($gender = null);
+            $newOrder->customer_surname = $faker->lastName();
+            $newOrder->customer_email = $faker->email();
+            $newOrder->customer_phone_number = $faker->phoneNumber();
+            $newOrder->customer_address = $faker->address();
+            $newOrder->save();
+        }
+    }
+}
