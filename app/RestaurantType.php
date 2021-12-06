e742cda5d0bbc7e748a461785df3406053f9f923
nicolafelice <felicenicola99@hotmail.com>
2021-12-06 22:16:28 +0100

    fix: models many to many relations

--- app/RestaurantType.php
+++ app/RestaurantType.php
@@ -14,6 +14,6 @@ class RestaurantType extends Model
 
     public function users()
     {
-        return $this->belongsToMany("App\User");
+        return $this->belongsToMany("App\User", 'user_restaurant_type');
     }
 }
