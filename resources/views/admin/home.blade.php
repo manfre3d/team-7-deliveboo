915aff3cfa366a18aa5d7c54a6f9af3375a4ebc2
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-12-07 15:15:31 +0100

    Fix: p.iva validation and description to not required in RegisterController. Text details:  (text language to Italian, added * to required fileds)

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Benvenuto nella tua Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
