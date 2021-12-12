9c8170e6a087ec52963524fbb797cb4dd48f06e7
manfred-commits <manfredipiraino@gmail.com>
2021-12-12 22:00:49 +0100

    fix: refactoring cart & restaurant dishes

--- resources/js/front/pages/Restaurant.vue
+++ resources/js/front/pages/Restaurant.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
-    <div class="container">
+  <section >
+    <div v-if="restaurant!=null">
 
       <div class="restaurant-info">
-        <h1>Ristorante{{this.restaurant.name}}</h1>
+        <h1>Ristorante{{restaurant.name}}</h1>
       </div>
 
       <h4>Menù</h4>
@@ -14,32 +14,61 @@
       </ul> -->
     </div>
 
-    <div class="plates" :v-if="restaurantId != 0">
-      <div class="plate" v-for="(plate, index) in plates" :key="index">
-        <h3>{{ plate.name }}</h3>
-        <div class="price">Prezzo: {{ plate.price }} €</div>
-        <button class="btn add-cart" @click="addToCart(plate)">
-          Aggiungi al carrello
-        </button>
+    <div class="d-flex">
+      <div class="plates d-flex col-6" >
+
+
+        <!-- lista di piatti del ristorante -->
+        <ul class="list-group"  style="width: 100%;">
+          <li class="list-group-item" v-for="(plate, index) in plates" :key="index">
+            <div v-if="plate.name" class="text_section_restaurant d-flex flex-wrap ">
+                <h4 style="width: 100%;">{{ plate.name }}</h4>
+                <div v-if="plate.price" class="price" style="width: 50%;">Prezzo: {{ plate.price.toFixed(2) }} €</div>
+                <button  class="btn add-cart" @click="addToCart(plate)" style="width: 50%;">
+                  Aggiungi al carrello
+                </button>
+            </div>
+            <!-- <img v-if="restaurant.img_path" :src="require('../img/seeder_images/' + restaurant.img_path)" alt="restaurant img"> -->
+          </li>
+        </ul>
+
+
+        <!-- versione a schede per i piatti -->
+        <!-- <div  class="plate col-4 card card-menu" v-for="(plate, index) in plates" :key="index">
+          <div v-if="plates!=0" class="card-body d-flex flex-column" >
+
+            <img v-if="plate.img_path" :src="require('../img/seeder_images/' + plate.img_path)" alt="restaurant img">
+            <div v-if="plate.name" class="text_section_restaurant d-flex flex-wrap ">
+                <h4>{{ plate.name }}</h4>
+                <div v-if="plate.price" class="price">Prezzo: {{ plate.price.toFixed(2) }} €</div>
+                <button class="btn add-cart" @click="addToCart(plate)">
+                  Aggiungi al carrello
+                </button>
+            </div>
+
+          </div> 
+          
+        </div>-->
       </div>
+      <div class="cart d-flex flex-column col-6">
+        <h2>Carrello</h2>
+        <ul class="cart-container">
+          <li v-for="(plate, index) in cart" :key="index">
+            <h4>{{ plate.name }}</h4>
+            <div>{{ plate.price.toFixed(2) }} €</div>
+            <button class="btn cart-remove" @click="removeToCart(plate.id)">
+              Rimuovi
+            </button>
+          </li>
+        </ul>
+        <div class="total-container">
+          <h2>Totale:</h2>
+          <span id="total-price">€{{ getTotalPrice() }}</span>
+        </div>
+      </div>
+
     </div>
     
-    <div class="cart">
-      <h2>Carrello</h2>
-      <ul class="cart-container">
-        <li v-for="(plate, index) in cart" :key="index">
-          <h4>{{ plate.name }}</h4>
-          <div>{{ plate.price }} €</div>
-          <button class="btn cart-remove" @click="removeToCart(plate.id)">
-            Rimuovi
-          </button>
-        </li>
-      </ul>
-      <div class="total-container">
-        <h2>Totale:</h2>
-        <span id="total-price">€{{ getTotalPrice() }}</span>
-      </div>
-    </div>
   </section>
 </template>
 
@@ -91,7 +120,12 @@ export default {
       .get(`/api/plates/${this.restaurantId}`)
       .then((response) => {
         console.log(response);
-        this.plates = response.data.data;
+        if(response.data.data=="no results found"){
+          this.plates=0;
+        }else{
+          this.plates = response.data.data;
+
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -102,7 +136,9 @@ export default {
 
   methods: {
     addToCart: function (plate) {
-      this.cart.push(plate);
+      if(!this.cart.includes(plate)){
+        this.cart.push(plate);
+      }
     },
     removeToCart: function (id) {
       // this.cart = this.cart.filter((elm) => {
@@ -126,8 +162,8 @@ export default {
       this.cart.forEach((elm) => {
         tot += elm.price;
       });
-
-      return tot;
+      
+      return tot.toFixed(2);
     },
   },
 };
@@ -139,6 +175,7 @@ export default {
 .plates {
 	display: flex;
 	flex-wrap: wrap;
+  padding:0;
   border: 3px solid black;
 }
 .cart {
@@ -149,9 +186,9 @@ export default {
 	text-align: center;
 	background-color: #00eb91;
 }
-.plate {
-	width: calc(100% / 5);
-}
+// .plate {
+// 	width: calc(100% / 5);
+// }
 
 .cart {
   border: 3px solid black;
