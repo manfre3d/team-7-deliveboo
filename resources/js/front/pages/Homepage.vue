c2457c4d7b64defc72efadf208fcd5856827315b
manfred-commits <manfredipiraino@gmail.com>
2021-12-12 13:33:48 +0100

    add: restaurant list component

--- resources/js/front/pages/Homepage.vue
+++ resources/js/front/pages/Homepage.vue
@@ -1,19 +1,41 @@
 <template>
 <div class="container">
-  <h1>sono la Homepage</h1>
+  <h1 class="text-center">Sono la Homepage</h1>
+  <h2>Categorie di Ristoranti</h2>
   <ul>
-    <RestaurantCategory/>
+    <RestaurantCategory @category="getCategory" />
   </ul>
+  <hr>
+  <h2>Ristoranti</h2>
+  <RestaurantsList :selectedCategory="category"/>
   </div>
 </template>
 
 <script>
 import RestaurantCategory from '../components/RestaurantCategory';
+import RestaurantsList from '../components/RestaurantsList';
+
 export default {
     components: {
-      RestaurantCategory
+      RestaurantCategory,
+      RestaurantsList,
+    },
+    data() {
+      return {
+        category: 0,
+        restaurants:[],
+        filteredRestaurants:[],
+
+      }
+    },
+    methods:{
+      getCategory(categorySelected){
+
+        // console.log(categorySelected);
+        this.category=categorySelected;
+
+      }
     }
-    
 }
 </script>
 
