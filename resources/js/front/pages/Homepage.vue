b476efcfadd5a65f26fa03290b47f96eaf0f27b4
eliaraguso <elia.raguso@gmail.com>
2021-12-13 16:08:11 +0100

    fix: rifatta grafica temporanea homepage

--- resources/js/front/pages/Homepage.vue
+++ resources/js/front/pages/Homepage.vue
@@ -1,45 +1,38 @@
 <template>
-<div class="container">
-  <h1 class="text-center">Sono la Homepage</h1>
-  <h2>Categorie di Ristoranti</h2>
-  <ul>
-    <RestaurantCategories @category="getCategory"/>    
-  </ul>
-  <hr>
-  <h2>Ristoranti</h2>
-  <RestaurantsList :selectedCategory="category"/>
+  <div class="container-xxl">
+    <Hero />
+    <RestaurantCategories @category="getCategory" />
+    <RestaurantsList :selectedCategory="category" />
   </div>
 </template>
 
 <script>
-import RestaurantsList from '../components/RestaurantsList';
-import RestaurantCategories from '../components/RestaurantCategories';
+import Hero from "../components/Hero.vue";
+
+import RestaurantsList from "../components/RestaurantsList";
+import RestaurantCategories from "../components/RestaurantCategories";
 
 export default {
-    components: {
-      RestaurantsList,
-      RestaurantCategories,
+  components: {
+    Hero,
+    RestaurantsList,
+    RestaurantCategories,
+  },
+  data() {
+    return {
+      category: 0,
+      restaurants: [],
+      filteredRestaurants: [],
+    };
+  },
+  methods: {
+    getCategory(categorySelected) {
+      // console.log(categorySelected);
+      this.category = categorySelected;
     },
-    data() {
-      return {
-        category: 0,
-        restaurants:[],
-        filteredRestaurants:[],
-
-      }
-    },
-    methods:{
-      getCategory(categorySelected){
-
-        // console.log(categorySelected);
-        this.category=categorySelected;
-
-      }
-
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
