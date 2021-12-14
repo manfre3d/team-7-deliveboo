ffffc6680787a9b40c987bc4213abcb17390d4cb
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-12-14 17:18:53 +0100

    Fix: view restaurant categories

--- resources/js/front/components/RestaurantCategories.vue
+++ resources/js/front/components/RestaurantCategories.vue
@@ -7,36 +7,21 @@
     </div>
 
     <ul class="row justify-content-center" style="margin-bottom: 20px">
-      <li
-        class="d-flex col col-md-6 col-lg-4 flex-grow-0 justify-content-center"
-        v-for="category in categories"
-        :key="category.id">
-        <div
-          class="card mx-1 card-menu"
-          style=" cursor: pointer"
-          @click="$emit('category', category.id)">
+      <li class="d-flex col col-md-6 col-lg-4 flex-grow-0 justify-content-center" v-for="category in categories" :key="category.id">
+        <div class="card mx-1 card-menu" style=" cursor: pointer" @click="$emit('category', category.id)">
           <div class="card-body d-flex flex-column">
-            <img
-              v-if="category.img_path"
-              :src="require('../img/restaurant_category/' + category.img_path)"
-              alt="category img"/>
-            {{ category.name }}
+            <img v-if="category.img_path" :src="require('../img/restaurant_category/' + category.img_path)" alt="category img">
+            <span>{{ category.name }}</span>
           </div>
         </div>
       </li>
-      <li
-        class="row justify-content-center"
-        style="width: 100%; margin-top: 20px"
-      >
-        <h4
-          class="d-flex flex-wrap justify-content-center mx-1"
-          v-for="category in categories"
-          :key="category.id"
-          @click="$emit('category', category.id)"
-        >
-          <router-link class="badge badge-warning" to="">{{
-            category.name
-          }}</router-link>
+
+      <!-- Stampa tag categorie -->
+      <li class="row justify-content-center tag">
+        <h4 class="d-flex flex-wrap justify-content-center mx-1" v-for="category in categories" :key="category.id" @click="$emit('category', category.id)">
+          <router-link class="badge" to="">
+            {{category.name}}
+          </router-link>
         </h4>
       </li>
     </ul>
@@ -70,55 +55,50 @@ export default {
 @import "../../../sass/front/variables.scss";
 
 .container {
-  // border: 5px solid red;
-  padding: 100px 0;
+  padding: 3.75rem 0;
 }
-// .container {
-//   border: 3px solid orange;
-//   border-radius: 15px;
-//   ul {
-//     display: flex;
-//     justify-content: space-between;
-//     list-style: none;
-//   }
-// }
 
 @mixin centered {
     max-width: 85%;
     margin: auto;
 }
+@mixin title{
+  font-size: 1.25rem;
+  font-weight: 700;
+  color: $titleColor;
+}
 .container{
   @include centered;
-  // margin-top: -150px;
   h2{
     color: $titleColor;
-    font-size: 50px;
+    font-size: 2.5rem;
     font-weight: 600;
-    margin-bottom: 40px;
+    padding-bottom: 1.87rem;
   }
   ul{
     width: 100%;
       li{
         list-style: none;
-      // border: 1px solid black;
-      // margin: 30px 0;
-      // background-color: hotpink;
-      // margin-bottom: 40px;
-      // padding: 20px 0;
     }
   }
   .card-body {
-    text-align: center;
-    font-size: 26px;
+    span{
+      @include title;
+      padding-top: .62rem;
+    }
+    
   }
   .card {
     border: none;
   }
-  
 }
-
-
-
-
-
+.tag{
+  h4{
+    .badge{
+      @include title;
+      background-color: #ececec;
+      margin-top: 3.75rem;
+    }
+  }
+}
 </style>
