32fa33b46ca69fe1123e2127d8e1a2993d7ca0d0
manfred-commits <manfredipiraino@gmail.com>
2021-12-27 15:31:51 +0100

    add: heroku proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -6,18 +6,18 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
-{
+{   
     /**
      * The trusted proxies for this application.
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies='*';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
