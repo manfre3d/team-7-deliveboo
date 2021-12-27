c15684e6b4009b945523206ee38ac8e47c3dbfb5
manfred-commits <manfredipiraino@gmail.com>
2021-12-27 22:33:21 +0100

    fix: braintree

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Providers\Braintree_Configuration;
 use Braintree;
 
 class PageController extends Controller
@@ -14,7 +15,7 @@ class PageController extends Controller
 
     public function checkout()
     {
-        $gateway = new Braintree\Gateway([
+        $gateway = new \Braintree\Gateway([
             'environment' => config('services.braintree.environment'),
             'merchantId' => config('services.braintree.merchantId'),
             'publicKey' => config('services.braintree.publicKey'),
